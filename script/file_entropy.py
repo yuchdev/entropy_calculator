cbac9278d74f0de57a31f3c4d18357c89743b9c4
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-04-20 13:08:08 +0700

    Add Python ProgressBar

--- script/file_entropy.py
+++ script/file_entropy.py
@@ -16,7 +16,7 @@ import itertools
 import struct
 
 
-def shennon_entropy(frequency_list):
+def shannon_entropy(frequency_list):
     ent = 0.0
     for freq in frequency_list:
         if freq > 0:
@@ -24,32 +24,48 @@ def shennon_entropy(frequency_list):
     return -ent
 
 
-# Print iterations progress
-def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '*'):
+# For printing iterations progress
+class ProgressBar:
     """
-    Call in a loop to create terminal progress bar
-    Usage example:
-    # Set 0%
-    progress_bar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    while some():
-        progress_bar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    Create command-line-style ProgressBar
+    """
+    def __init__(self, total, prefix='', suffix='', length=100, fill='*'):
+        """
+        Init ProgressBar
+        Start position is always 0, end position is total.
+        Usage example:
+        progress_bar = ProgressBar(0, l, prefix='Progress:', suffix='Complete', length=50)
+        while some():
+            progress_bar.iterate(i + 1)
+        :param total: Required, total iterations (Int)
+        :param prefix: Optional, prefix string (Str)
+        :param suffix: Optional, suffix string (Str)
+        :param length: Optional, character length of bar (Int)
+        :param fill: Optional, bar fill character (Str)
+        """
+        self.counter = 0
+        self.prev_counter = 0
+        self.total = total
+        self.percent_size = total/100
+        self.prefix = prefix
+        self.suffix = suffix
+        self.length = length
+        self.fill = fill
 
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
+    def iterate(self, iteration):
+        """
+        Call in a loop to create terminal progress bar
+        @param: iteration: Required, current iteration (Int)
+        """
+        self.prev_counter = int(iteration/self.percent_size)
+        filled_length = int((self.prev_counter/100) * self.length)
+        bar = self.fill * filled_length + '-' * (self.length - filled_length)
+        if self.prev_counter != self.counter:
+            print('\r%s |%s| %s%% %s' % (self.prefix, bar, self.prev_counter, self.suffix), end='\r')
+        self.counter = self.prev_counter
+        # Print New Line on Complete
+        if iteration == self.total:
+            print()
 
 
 # Shannon entropy
@@ -63,7 +79,8 @@ if __name__ == '__main__':
 
     file_size = os.stat(file_name).st_size
     print('File size in bytes: {0}'.format(file_size))
-    percent_size = file_size / 100
+
+    progress_bar = ProgressBar(file_size, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     bytes_list = list(itertools.repeat(0, 256))
     with open(file_name, "rb") as f:
@@ -75,16 +92,14 @@ if __name__ == '__main__':
             value = struct.unpack('B', read_byte)[0]
             bytes_list[value] += 1
             counter += 1
-            if counter > percent_size:
-                counter = 0
-                percent_counter += 1
-                print('{0} % read'.format(percent_counter))
+            # Check if percentage changed not to re-print on every iteration, print() is heavy operation
+            progress_bar.iterate(counter)
             read_byte = f.read(1)
 
     # divide by file size
     frequency_list = [float(elem)/file_size for elem in bytes_list]
 
-    entropy = shennon_entropy(frequency_list)
+    entropy = shannon_entropy(frequency_list)
     print('Shannon entropy (min bits per byte-character): {0}'.format(entropy))
     print('Min possible file size assuming max theoretical compression efficiency:')
     print('{0} in bits'.format(entropy * file_size))
